fa43054e45ff985b4579234a3aaed66f87bbd1b3
김원태 <rladnjsxo123@naver.com>
2025-03-04 16:52:59 +0900

    240 Link 컴포넌트로 네비게이팅하기

--- starting-app_v1/pages/index.js
+++ starting-app_v1/pages/index.js
@@ -1,7 +1,23 @@
+import Link from "next/link";
 import React from "react";
 
 const HomePage = (props) => {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <ul>
+        <li>
+          <Link href="/portfolio">Portfolio</Link>
+        </li>
+        <li>
+          <Link href="/clients">clients</Link>
+        </li>
+        <li>
+          <Link href="/blog/123/213">Go blog 123/123</Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default HomePage;
