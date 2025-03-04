ff770187d87047fd94b2ac068f2888ff9b6b3dce
김원태 <rladnjsxo123@naver.com>
2025-03-04 16:11:18 +0900

    233 homepage

--- starting-app_v1/pages/index.js
+++ starting-app_v1/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-const index = (props) => {
-  return <div>index</div>;
+const HomePage = (props) => {
+  return <div>Home Page</div>;
 };
 
-export default index;
+export default HomePage;
