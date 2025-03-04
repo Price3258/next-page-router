b9c52c6da0fe30d5489bd9d314922c5f7350251b
김원태 <rladnjsxo123@naver.com>
2025-03-04 16:49:15 +0900

    239 catch-all 라우트 추가하기

---
+++ starting-app_v1/pages/blog/[...slug].js
@@ -0,0 +1,9 @@
+import { useRouter } from "next/router";
+import React from "react";
+
+export default function BlogPostsPage() {
+  const router = useRouter();
+  console.log(router.query);
+
+  return <div>BlogPostsPage</div>;
+}
