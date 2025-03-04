bfc12ac1a40c496480c34bfbb0b81395dc815a60
김원태 <rladnjsxo123@naver.com>
2025-03-04 21:31:33 +0900

    261 Catch-all 페이지에서 데아터 추출하기

--- prj-routing-starting-setup/pages/events/[...slug].js
+++ prj-routing-starting-setup/pages/events/[...slug].js
@@ -1,6 +1,42 @@
+import { getFilteredEvents } from "@/dummy-data";
+import { useRouter } from "next/router";
 import React from "react";
 
 export default function FilteredEventsPage() {
+  const router = useRouter();
+  const { query } = router;
+  const filterData = query.slug;
+
+  if (!filterData) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return <p>Invalid filter</p>;
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p>No events found for the chosen filter!</p>;
+  }
+
   return (
     <div>
       <h1>Filtered Events Page</h1>
