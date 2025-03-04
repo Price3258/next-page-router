e43b7c5757eceae758cb94cb5f342475ab8eade4
김원태 <rladnjsxo123@naver.com>
2025-03-04 20:40:42 +0900

    252 이벤트 리스트 작업

---
+++ prj-routing-starting-setup/components/events/event-item.js
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import React from "react";
+
+export default function EventItem(props) {
+  const { title, image, date, location, id } = props;
+
+  const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+
+  const formattedAddress = location.replace(", ", "\n");
+
+  const exploreLink = `/events/${id}`;
+
+  return (
+    <li>
+      <img src={"/" + image} alt={title} />
+      <div>
+        <div>
+          <h2>{title}</h2>
+          <div>
+            <time>{humanReadableDate}</time>
+          </div>
+          <address>{formattedAddress}</address>
+        </div>
+        <div>
+          <Link href={exploreLink}>Explore event</Link>
+        </div>
+      </div>
+    </li>
+  );
+}
--- prj-routing-starting-setup/pages/index.js
+++ prj-routing-starting-setup/pages/index.js
@@ -1,9 +1,13 @@
+import EventList from "@/components/events/event-list";
+import { getFeaturedEvents } from "@/dummy-data";
 import React from "react";
 
 export default function HomePage() {
+  const featuredEvents = getFeaturedEvents();
+
   return (
     <div>
-      <h1>Home Page</h1>
+      <EventList items={featuredEvents} />
     </div>
   );
 }
