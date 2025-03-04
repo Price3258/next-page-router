af74b2f90985912ee1ae90d893ee3d8670350f76
김원태 <rladnjsxo123@naver.com>
2025-03-04 21:02:46 +0900

    256 Event detail 페이지 추가하기 (동적라우트)

--- prj-routing-starting-setup/pages/events/[eventId].js
+++ prj-routing-starting-setup/pages/events/[eventId].js
@@ -1,9 +1,32 @@
+import { useRouter } from "next/router";
 import React from "react";
 
+import EventSummary from "@/components/event-detail/event-summary";
+import { getEventById } from "@/dummy-data";
+import EventLogistics from "@/components/event-detail/event-logistics";
+import EventContent from "@/components/event-detail/event-content";
+
 export default function EventDetailPage() {
+  const router = useRouter();
+  const { eventId } = router.query;
+
+  const event = getEventById(eventId);
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
   return (
-    <div>
-      <h1>Event Detail page</h1>
-    </div>
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
   );
 }
