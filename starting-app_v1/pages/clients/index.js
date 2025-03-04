cca42d4d1c7f6a8e1183e37495c14590ae486e23
김원태 <rladnjsxo123@naver.com>
2025-03-04 16:58:22 +0900

    242 Link  href 설정하는 다른 방법

--- starting-app_v1/pages/clients/index.js
+++ starting-app_v1/pages/clients/index.js
@@ -12,7 +12,16 @@ export default function ClientPage() {
       <ul>
         {clients.map((client) => (
           <li key={client.id}>
-            <Link href={`/clients/${client.name}`}>{client.name}</Link>
+            <Link
+              href={{
+                pathname: "/clients/[id]",
+                query: {
+                  id: client.id,
+                },
+              }}
+            >
+              {client.name}
+            </Link>
           </li>
         ))}
       </ul>
