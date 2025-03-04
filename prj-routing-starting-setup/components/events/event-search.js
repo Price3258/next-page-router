9d29fa63b1604ab7c82d51b8fc1b3581cf1975c1
김원태 <rladnjsxo123@naver.com>
2025-03-04 21:24:17 +0900

    260 Filtered Events 페이지를 프로그래밍 방식으로 네비게이팅하기

--- prj-routing-starting-setup/components/events/event-search.js
+++ prj-routing-starting-setup/components/events/event-search.js
@@ -1,21 +1,33 @@
-import React from "react";
+import React, { useRef } from "react";
+
 import Button from "../ui/button";
 import classes from "./events-search.module.css";
 
 export default function EventSearch(props) {
+  const yearInputRef = useRef();
+  const monthInputRef = useRef();
+
+  function submitHanlder(event) {
+    event.preventDefault();
+    const selectedYear = yearInputRef.current.value;
+    const selectedMonth = monthInputRef.current.value;
+
+    props.onSearch(selectedYear, selectedMonth);
+  }
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHanlder}>
       <div className={classes.controls}>
         <div className={classes.control}>
           <label htmlFor="year">Year</label>
-          <select id="year">
+          <select id="year" ref={yearInputRef}>
             <option value="2021">2021</option>
             <option value="2022">2022</option>
           </select>
         </div>
         <div className={classes.control}>
           <label htmlFor="month">Month</label>
-          <select id="month">
+          <select id="month" ref={monthInputRef}>
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
--- prj-routing-starting-setup/pages/index.js
+++ prj-routing-starting-setup/pages/index.js
@@ -1,14 +1,22 @@
 import EventList from "@/components/events/event-list";
 import EventSearch from "@/components/events/event-search";
 import { getFeaturedEvents } from "@/dummy-data";
+import { useRouter } from "next/router";
 import React from "react";
 
 export default function HomePage() {
+  const router = useRouter();
+
   const featuredEvents = getFeaturedEvents();
+
+  function findEventHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
 
   return (
     <>
-      <EventSearch />
+      <EventSearch onSearch={findEventHandler} />
       <EventList items={featuredEvents} />
     </>
   );
