5ea0fe689c29e70961c8c9de2835719173073956
김원태 <rladnjsxo123@naver.com>
2025-03-06 13:51:50 +0900

    297 홈페이지에 덩적사이트 생성 추가하기

--- prj-routing-starting-setup/pages/events/index.js
+++ prj-routing-starting-setup/pages/events/index.js
@@ -1,12 +1,21 @@
 import EventList from "@/components/events/event-list";
-import { getAllEvents } from "@/dummy-data";
+import { getAllEvents } from "@/helpers/api-util";
 import React from "react";
 
-export default function AllEventsPage() {
-  const events = getAllEvents();
+export default function AllEventsPage(props) {
+  const { events } = props;
   return (
     <div>
       <EventList items={events} />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+  return {
+    props: {
+      events,
+    },
+  };
 }
--- prj-routing-starting-setup/pages/index.js
+++ prj-routing-starting-setup/pages/index.js
@@ -1,13 +1,13 @@
 import EventList from "@/components/events/event-list";
 import EventSearch from "@/components/events/event-search";
-import { getFeaturedEvents } from "@/dummy-data";
+import { getFeaturedEvents } from "@/helpers/api-util";
 import { useRouter } from "next/router";
 import React from "react";
 
-export default function HomePage() {
+export default function HomePage(props) {
   const router = useRouter();
 
-  const featuredEvents = getFeaturedEvents();
+  const { featuredEvents } = props;
 
   function findEventHandler(year, month) {
     const fullPath = `/events/${year}/${month}`;
@@ -20,4 +20,13 @@ export default function HomePage() {
       <EventList items={featuredEvents} />
     </>
   );
+}
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+  return {
+    props: {
+      featuredEvents,
+    },
+  };
 }
