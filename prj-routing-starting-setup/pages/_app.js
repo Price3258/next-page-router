798241d443997619d627fdc9513189982584dd58
김원태 <rladnjsxo123@naver.com>
2025-03-06 20:34:27 +0900

    347 알림을 위한 context 생성

--- prj-routing-starting-setup/pages/_app.js
+++ prj-routing-starting-setup/pages/_app.js
@@ -1,17 +1,27 @@
-import Layout from "@/components/layout/layout";
-import "@/styles/globals.css";
 import Head from "next/head";
+
+import "@/styles/globals.css";
+
+import Layout from "@/components/layout/layout";
+import Notification from "@/components/ui/notification";
+import { NotificationContextProvider } from "@/store/notification-context";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Head>
-        <title>Next Events</title>
-        <meta name="description" content="Nextjs events" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <Component {...pageProps} />
-    </Layout>
+    <NotificationContextProvider>
+      <Layout>
+        <Head>
+          <title>Next Events</title>
+          <meta name="description" content="Nextjs events" />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
+        <Component {...pageProps} />
+        <Notification title="test" message="success" status="success" />
+      </Layout>
+    </NotificationContextProvider>
   );
 }
 
