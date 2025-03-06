5dcce14e2a4cc28ed14c308988adbfa648638551
김원태 <rladnjsxo123@naver.com>
2025-03-06 19:13:03 +0900

    336 mongoDB 연결해서 유저등록해보기

--- prj-routing-starting-setup/pages/api/newsletter.js
+++ prj-routing-starting-setup/pages/api/newsletter.js
@@ -1,4 +1,7 @@
-function handler(req, res) {
+import { MONGO_DB_CLIENT } from "@/lib/constant";
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const userEmail = req.body.email;
     if (!userEmail || !userEmail.includes("@")) {
@@ -6,6 +9,13 @@ function handler(req, res) {
       return;
     }
     console.log(userEmail);
+
+    const client = await MongoClient.connect(MONGO_DB_CLIENT);
+    const db = client.db();
+    await db.collection("emails").insertOne({ email: userEmail });
+
+    client.close();
+
     res.status(201).json({ message: "Signed Up!" });
   }
 }
