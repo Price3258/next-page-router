04e6ee30183251ddf062bed610dd8c9217ec7ab8
김원태 <rladnjsxo123@naver.com>
2025-03-06 16:32:18 +0900

    322 입력요청 구문 분석하기 post api 만들기

--- nextjs-api-routes/pages/index.js
+++ nextjs-api-routes/pages/index.js
@@ -1,7 +1,32 @@
+import { useRef } from "react";
+
 function HomePage() {
+  const emailRef = useRef();
+  const feedbackRef = useRef();
+
+  function submitFormHandler(event) {
+    event.preventDefault(); // block reloading
+
+    const email = emailRef.current.value;
+    const feedback = feedbackRef.current.value;
+
+    fetch("localhost://3000/api/feedback");
+  }
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onClick={submitFormHandler}>
+        <div>
+          <label htmlFor="email">Your email</label>
+          <input id="email" type="email" ref={emailRef} />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your Feedback</label>
+          <textarea id="feedback" rows={5} ref={feedbackRef} />
+        </div>
+        <button>Send Feedback</button>
+      </form>
     </div>
   );
 }
