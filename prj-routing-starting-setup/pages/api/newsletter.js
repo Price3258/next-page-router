b95c0043613d33cf8565717037c598cd57dc55d2
김원태 <rladnjsxo123@naver.com>
2025-03-06 19:47:37 +0900

    339. 에러 핸들링 추가하기

--- prj-routing-starting-setup/pages/api/newsletter.js
+++ prj-routing-starting-setup/pages/api/newsletter.js
@@ -1,6 +1,16 @@
 import { getMongoDBClient } from "@/lib/db";
 import { MongoClient } from "mongodb";
 
+async function connectDatabase() {
+  const client = await MongoClient.connect(getMongoDBClient("newsletter"));
+  return client;
+}
+
+async function insertDocument(client, document) {
+  const db = client.db();
+  await db.collection("emails").insertOne(document);
+}
+
 async function handler(req, res) {
   if (req.method === "POST") {
     const userEmail = req.body.email;
@@ -10,12 +20,20 @@ async function handler(req, res) {
     }
     console.log(userEmail);
 
-    const client = await MongoClient.connect(getMongoDBClient("newsletter"));
-    const db = client.db();
-    await db.collection("emails").insertOne({ email: userEmail });
-
-    client.close();
-
+    let client;
+    try {
+      client = await connectDatabase();
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Connecting to db failed!" });
+    }
+    try {
+      await insertDocument(client, { email: userEmail });
+      client.close();
+    } catch (error) {
+      console.llog(error);
+      res.status(500).json({ message: "Inserting data failed!" });
+    }
     res.status(201).json({ message: "Signed Up!" });
   }
 }
