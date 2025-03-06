ceb2ff4a2ba7054740894cbd7b54f909b18d4abf
김원태 <rladnjsxo123@naver.com>
2025-03-06 21:03:51 +0900

    352 코멘트에 로딩추가

--- prj-routing-starting-setup/components/input/comments.js
+++ prj-routing-starting-setup/components/input/comments.js
@@ -1,22 +1,26 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import CommentList from "./comment-list";
 import NewComment from "./new-comment";
 import classes from "./comments.module.css";
+import NotificationContext from "@/store/notification-context";
 
 function Comments(props) {
   const { eventId } = props;
+  const notificationCtx = useContext(NotificationContext);
 
   const [showComments, setShowComments] = useState(false);
   const [comments, setComments] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     if (showComments) {
+      setIsLoading(true);
       fetch(`/api/comments/${eventId}`)
         .then((res) => res.json())
         .then((data) => {
-          console.log(data);
           setComments(data.comments);
+          setIsLoading(false);
         });
     }
   }, [showComments]);
@@ -26,7 +30,12 @@ function Comments(props) {
   }
 
   function addCommentHandler(commentData) {
-    // send data to API
+    notificationCtx.showNotification({
+      title: "Sending comments",
+      message: "Your comment is currently being stored into a db",
+      status: "pending",
+    });
+
     fetch(`/api/comments/${eventId}`, {
       method: "POST",
       body: JSON.stringify(commentData),
@@ -35,7 +44,20 @@ function Comments(props) {
       },
     })
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => {
+        notificationCtx.showNotification({
+          title: "Success",
+          message: "Your comment is successfully registered",
+          status: "success",
+        });
+      })
+      .catch((error) => {
+        notificationCtx.showNotification({
+          title: "Error!",
+          message: error.message || "something is wrong!",
+          status: "error",
+        });
+      });
   }
 
   return (
@@ -44,7 +66,8 @@ function Comments(props) {
         {showComments ? "Hide" : "Show"} Comments
       </button>
       {showComments && <NewComment onAddComment={addCommentHandler} />}
-      {showComments && <CommentList items={comments} />}
+      {showComments &&
+        (isLoading ? <div>Loading...</div> : <CommentList items={comments} />)}
     </section>
   );
 }
