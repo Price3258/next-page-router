7c23c0a00ee68338141f910baf07cfe6c6e4c6de
김원태 <rladnjsxo123@naver.com>
2025-03-06 20:37:37 +0900

    348. 콘텍스트 상태 추가하기

--- prj-routing-starting-setup/store/notification-context.js
+++ prj-routing-starting-setup/store/notification-context.js
@@ -1,14 +1,31 @@
-const { createContext } = require("react");
+const { createContext, useState } = require("react");
 
 const NotificationContext = createContext({
   notification: null, // {title, message, status},
-  showNotification: function () {},
+  showNotification: function (data) {},
   hideNotification: function () {},
 });
 
 export function NotificationContextProvider({ children }) {
+  const [activeNotification, setActiveNotification] = useState(false);
+
+  function showNotificationHandler(data) {
+    setActiveNotification(data);
+  }
+  function hideNotificationHanlder() {
+    setActiveNotification(null);
+  }
+
+  const context = {
+    notification: activeNotification,
+    showNotification: showNotificationHandler,
+    hideNotification: hideNotificationHanlder,
+  };
+
   return (
-    <NotificationContext.Provider>{children}</NotificationContext.Provider>
+    <NotificationContext.Provider value={context}>
+      {children}
+    </NotificationContext.Provider>
   );
 }
 
