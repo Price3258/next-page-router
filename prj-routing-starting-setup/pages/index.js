836dcc3a109c85a921729488672980fff664b66b
김원태 <rladnjsxo123@naver.com>
2025-03-06 15:09:49 +0900

    306 head 메타데이터의 필요성 분석하기

--- prj-routing-starting-setup/pages/index.js
+++ prj-routing-starting-setup/pages/index.js
@@ -1,11 +1,18 @@
 import EventList from "@/components/events/event-list";
 import { getFeaturedEvents } from "@/helpers/api-util";
+import Head from "next/head";
 
 export default function HomePage(props) {
   const { featuredEvents } = props;
 
   return (
     <>
+      <div>
+        <Head>
+          <title>Nextjs Events</title>
+          <meta name="description" content="Find a lot of news" />
+        </Head>
+      </div>
       <EventList items={featuredEvents} />
     </>
   );
