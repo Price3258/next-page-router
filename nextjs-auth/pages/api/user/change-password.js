98a2119b1d4a466bbc9cc09fb3dd4edff9419345
김원태 <rladnjsxo123@naver.com>
2025-03-11 15:38:10 +0900

    413. “비밀번호 변경” 논리 추가하기

--- nextjs-auth/pages/api/user/change-password.js
+++ nextjs-auth/pages/api/user/change-password.js
@@ -1,5 +1,9 @@
 import { getSession } from "next-auth/react";
 
+import { MONGODB_URL } from "@/lib/constant";
+import { connectToDatabase } from "@/lib/db";
+import { hashPassword, verifyPassword } from "@/lib/auth";
+
 async function handler(req, res) {
   if (req.method !== "PATCH") {
     return;
@@ -7,9 +11,40 @@ async function handler(req, res) {
 
   const session = await getSession({ req: req });
   if (!session) {
-    res.status(401).json({ message: " Not authenticated!" });
+    res.status(401).json({ message: "Not authenticated!" });
     return;
   }
+  const userEmail = session.user.email;
+  const oldPassword = req.body.oldPassword;
+  const newPassword = req.body.newPassword;
+  const client = await connectToDatabase(MONGODB_URL);
+
+  const usersCollection = client.db().collection("users");
+  const user = usersCollection.findOne({ email: userEmail });
+
+  if (!user) {
+    res.status(404).json({ message: "User not found." });
+    return;
+  }
+
+  const currentPassword = user.password;
+
+  const passwordsAreEqual = await verifyPassword(oldPassword, currentPassword);
+
+  if (!passwordsAreEqual) {
+    res.status(403).json({ message: "Invalid password." });
+    client.close();
+    return;
+  }
+  const hashedPassword = await hashPassword(newPassword);
+
+  const result = await usersCollection.updateOne(
+    { email: userEmail },
+    { $set: { password: hashedPassword } }
+  );
+
+  client.close();
+  res.status(200).json({ message: "Password updated!", result });
 }
 
 export default handler;
