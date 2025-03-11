1d22e9538857726504c59116aa3919d6fd5dadfc
김원태 <rladnjsxo123@naver.com>
2025-03-11 15:58:19 +0900

    414. 프론트엔드로부터 “비밀번호 변경” 요청 전송하기

--- nextjs-auth/pages/api/user/change-password.js
+++ nextjs-auth/pages/api/user/change-password.js
@@ -1,29 +1,35 @@
-import { getSession } from "next-auth/react";
+import { getServerSession } from "next-auth";
 
-import { MONGODB_URL } from "@/lib/constant";
-import { connectToDatabase } from "@/lib/db";
 import { hashPassword, verifyPassword } from "@/lib/auth";
+import { connectToDatabase } from "@/lib/db";
+import { MONGODB_URL } from "@/lib/constant";
+import { authOptions } from "../auth/[...nextauth]";
 
 async function handler(req, res) {
   if (req.method !== "PATCH") {
     return;
   }
 
-  const session = await getSession({ req: req });
+  const session = await getServerSession(req, res, authOptions);
+
   if (!session) {
     res.status(401).json({ message: "Not authenticated!" });
     return;
   }
+
   const userEmail = session.user.email;
   const oldPassword = req.body.oldPassword;
   const newPassword = req.body.newPassword;
+
   const client = await connectToDatabase(MONGODB_URL);
 
   const usersCollection = client.db().collection("users");
-  const user = usersCollection.findOne({ email: userEmail });
+
+  const user = await usersCollection.findOne({ email: userEmail });
 
   if (!user) {
     res.status(404).json({ message: "User not found." });
+    client.close();
     return;
   }
 
@@ -36,6 +42,7 @@ async function handler(req, res) {
     client.close();
     return;
   }
+
   const hashedPassword = await hashPassword(newPassword);
 
   const result = await usersCollection.updateOne(
@@ -44,7 +51,7 @@ async function handler(req, res) {
   );
 
   client.close();
-  res.status(200).json({ message: "Password updated!", result });
+  res.status(200).json({ message: "Password updated!" });
 }
 
 export default handler;
