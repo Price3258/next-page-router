41ef5fc45c5c793e278566300c84ca19617317e4
김원태 <rladnjsxo123@naver.com>
2025-03-11 14:49:22 +0900

    403. “크리덴셜 인증 제공자” & 사용자 로그인 논리 추가하기

---
+++ nextjs-auth/pages/api/auth/[...nextauth].js
@@ -0,0 +1,76 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+import { connectToDatabase } from "@/lib/db";
+import { MONGODB_URL } from "@/lib/constant";
+import { verifyPassword } from "@/lib/auth";
+
+export const authOptions = {
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "text",
+          placeholder: "example@example.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Email and password are required.");
+        }
+
+        const client = await connectToDatabase(MONGODB_URL);
+        const usersCollection = client.db().collection("users");
+
+        const user = await usersCollection.findOne({
+          email: credentials.email,
+        });
+
+        if (!user) {
+          client.close();
+          throw new Error("No user found!");
+        }
+
+        const isValid = await verifyPassword(
+          credentials.password,
+          user.password
+        );
+
+        if (!isValid) {
+          client.close();
+          throw new Error("Could not log you in!");
+        }
+
+        client.close();
+        return { id: user._id.toString(), email: user.email };
+      },
+    }),
+  ],
+  // pages: {
+  //   signIn: "/auth/signin", // 커스텀 로그인 페이지가 있다면 설정 가능
+  // },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.email = user.email;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.user.id = token.id;
+        session.user.email = token.email;
+      }
+      return session;
+    },
+  },
+};
+
+export default NextAuth(authOptions);
